4c120bb7cf1801325d70c81edbba0db259a9de27
SkySarDev <skysar@ya.ru>
2022-08-31 19:53:38 +0300

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  poweredByHeader: false,
+  images: {
+    domains: ['media.rawg.io'],
+  },
 }
 
 module.exports = nextConfig
