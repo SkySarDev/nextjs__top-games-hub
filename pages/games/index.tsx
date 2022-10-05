99caa91333677458da8ec727fab7761c53aa6260
SkySarDev <skysar@ya.ru>
2022-10-05 17:18:17 +0300

    feat: add infinite scroll for games page

--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -5,15 +5,28 @@ import { PagesServices } from '@services/pages.services'
 import { customFetchQuery } from '@utils/fetch.utils'
 import { ContentError } from '@components/content'
 import { GamesContent } from '@components/pages/games'
+import { useInfiniteData } from '@hooks/useInfiniteData'
 
 const Games: NextPage = () => {
   const { data } = useQuery(['games-page'], PagesServices.getGames)
+  const { list, nextPage, nextPageError, fetchNextPage } = useInfiniteData({
+    initList: data?.games_list,
+    initNextPage: data?.next_page,
+  })
 
-  // prettier-ignore
   return (
     <>
       {data ? (
-        <GamesContent data={data} />
+        <GamesContent
+          title={data.title}
+          description={data.description}
+          background_image={data.background_image}
+          games_count={data.games_count}
+          games_list={list}
+          getNextPage={fetchNextPage}
+          next_page={nextPage}
+          nextPageError={nextPageError}
+        />
       ) : (
         <ContentError />
       )}
