6f639a09b387879c9606b990ba6ec43d408b4d7d
SkySarDev <skysar@ya.ru>
2022-10-05 18:18:39 +0300

    feat: add infinite scroll for developers page

--- pages/developers/[developerid].tsx
+++ pages/developers/[developerid].tsx
@@ -4,6 +4,8 @@ import { useQuery } from '@tanstack/react-query'
 
 import { PagesServices } from '@services/pages.services'
 import { customFetchQuery } from '@utils/fetch.utils'
+import { useInfiniteData } from '@hooks/useInfiniteData'
+import { ICardGame } from '@appTypes/cards.types'
 import { ContentError } from '@components/content'
 import { CategoryContentTemplate } from '@components/pages/common'
 
@@ -13,12 +15,26 @@ const DeveloperPage: NextPage = () => {
   const { data } = useQuery(['developer-by-id', developerId], () =>
     PagesServices.getDeveloperById(developerId)
   )
+  const { list, nextPage, nextPageError, fetchNextPage } =
+    useInfiniteData<ICardGame>({
+      initList: data?.games_list,
+      initNextPage: data?.next_page,
+    })
 
-  // prettier-ignore
   return (
     <>
       {data ? (
-        <CategoryContentTemplate data={data} />
+        <CategoryContentTemplate
+          title={data.title}
+          description={data.description}
+          description_raw={data.description_raw}
+          background_image={data.background_image}
+          games_count={data.games_count}
+          games_list={list}
+          next_page={nextPage}
+          nextPageError={nextPageError}
+          getNextPage={fetchNextPage}
+        />
       ) : (
         <ContentError />
       )}
--- pages/developers/index.tsx
+++ pages/developers/index.tsx
@@ -3,17 +3,32 @@ import { useQuery } from '@tanstack/react-query'
 
 import { customFetchQuery } from '@utils/fetch.utils'
 import { PagesServices } from '@services/pages.services'
+import { useInfiniteData } from '@hooks/useInfiniteData'
+import { ICardCommon } from '@appTypes/cards.types'
 import { ContentError } from '@components/content'
 import { CategoryListTemplate } from '@components/pages/common'
 
 const Developers: NextPage = () => {
   const { data } = useQuery(['developers-page'], PagesServices.getDevelopers)
+  const { list, nextPage, nextPageError, fetchNextPage } =
+    useInfiniteData<ICardCommon>({
+      initList: data?.list,
+      initNextPage: data?.next_page,
+    })
 
-  // prettier-ignore
   return (
     <>
       {data ? (
-        <CategoryListTemplate category='developers' data={data} />
+        <CategoryListTemplate
+          category="developers"
+          title={data.title}
+          description={data.description}
+          background_image={data.background_image}
+          list={list}
+          getNextPage={fetchNextPage}
+          next_page={nextPage}
+          nextPageError={nextPageError}
+        />
       ) : (
         <ContentError />
       )}
