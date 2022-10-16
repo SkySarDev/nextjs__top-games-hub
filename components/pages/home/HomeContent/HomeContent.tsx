8d2d5d8041fea17e86f5ea9bb1b8a47c72ebada1
SkySarDev <skysar@ya.ru>
2022-10-16 15:56:46 +0300

    feat: update bg image for home page

--- components/pages/home/HomeContent/HomeContent.tsx
+++ components/pages/home/HomeContent/HomeContent.tsx
@@ -1,27 +1,30 @@
 import { FC } from 'react'
 
 import { IHomePageResponse } from '@appTypes/homePage.types'
+import { IBgImage } from '@appTypes/base.types'
+import { ROUTES } from '@constants/routes.constants'
 import { MainHead } from '@components/layout'
 import { ContentHeader, ContentWrapper } from '@components/content'
-import { ContentRows } from '@styles/components/content.components'
 import { HomeSection } from '@components/pages/home'
-import { ROUTES } from '@constants/routes.constants'
 import {
   CardGamesListWithData,
   CardCommonsListWithData,
 } from '@components/cards'
+import { ContentRows } from '@styles/components/content.components'
 
 interface IHomeContentProps {
   data: IHomePageResponse
+  bgImage: IBgImage
 }
 
 const HomeContent: FC<IHomeContentProps> = ({
   data: { title, description, top_games, new_releases, popular_genres, tags },
+  bgImage,
 }) => {
   return (
     <>
       <MainHead title={title} description={description} />
-      <ContentHeader />
+      <ContentHeader image={bgImage.image} blurDataURL={bgImage.blurDataURL} />
 
       <ContentWrapper title={title}>
         <ContentRows>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,18 @@ import { useQuery } from '@tanstack/react-query'
 
 import { PagesServices } from '@services/pages.services'
 import { customFetchQuery } from '@utils/fetch.utils'
+import { IPageWithBgImage } from '@appTypes/pages.types'
 import { ContentError } from '@components/content'
 import { HomeContent } from '@components/pages/home'
 
-const Home: NextPage = () => {
+const Home: NextPage<IPageWithBgImage> = ({ bgImage }) => {
   const { data } = useQuery(['home-page'], PagesServices.getHome)
 
   // prettier-ignore
   return (
     <>
       {data ? (
-        <HomeContent data={data}/>
+        <HomeContent data={data} bgImage={bgImage}/>
       ) : (
         <ContentError />
       )}
