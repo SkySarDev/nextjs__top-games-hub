7311ffe744cf50c36ee301b155fbbcc0f8445b30
SkySarDev <skysar@ya.ru>
2022-10-18 17:50:20 +0300

    ui: remove border radius for blocks

--- components/shared/BlockWrapper/BlockWrapper.tsx
+++ components/shared/BlockWrapper/BlockWrapper.tsx
@@ -1,11 +1,7 @@
-import { ReactNode, FC } from 'react'
+import { FC, PropsWithChildren } from 'react'
 import styled from 'styled-components'
 
-interface IBlockWrapperProps {
-  children: ReactNode
-}
-
-const BlockWrapper: FC<IBlockWrapperProps> = ({ children }) => {
+const BlockWrapper: FC<PropsWithChildren> = ({ children }) => {
   return <Wrapper>{children}</Wrapper>
 }
 
@@ -14,6 +10,5 @@ export default BlockWrapper
 const Wrapper = styled.div`
   background: rgba(36, 38, 33, 0.8);
   border: 1px solid #59584c;
-  border-radius: 5px;
   padding: 20px;
 `
--- styles/components/cards.components.ts
+++ styles/components/cards.components.ts
@@ -8,7 +8,6 @@ export const CardBaseWrapper = styled.div<ICardBaseWrapperProps>`
   position: relative;
   height: ${({ height }) => height}px;
   border: 1px solid #59584c;
-  border-radius: 5px;
   background-color: #1b1b1b;
   overflow: hidden;
 `
